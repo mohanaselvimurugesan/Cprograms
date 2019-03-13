0f3f0db74efad80fc414b7a6430c99cb549b68b0
mohanaselvimurugesan <47887164+mohanaselvimurugesan@users.noreply.github.com>
2019-03-13 15:57:23 +0530

    Rename set1_3.c to set1_311.c

---
+++ set1_311.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int rev=0,n,a,i,sum=1;
+scanf("%d",&n);
+int temp=n;
+for(i=1;temp!=0;i++)
+{
+    a=temp%10;
+    rev=rev*10+a;
+    temp=temp/10;
+}
+printf("%d",rev);
+getch();
+return 0;
+}
